bf93a763a70ab03fb5c0c9205429e8efcde744bb
Raul Guiu <raulguiu@gmail.com>
2015-02-14 15:55:52 +0100

    decading multipliers

--- src/main/java/rg/competition/impl/trial2014/ExecutableImpl.java
+++ src/main/java/rg/competition/impl/trial2014/ExecutableImpl.java
@@ -30,64 +30,65 @@ public class ExecutableImpl implements Executable{
 
     @Override
     public String execute() {
-        while(!finished()) {
-            int[] currentWinner = new int[]{-1,-1,-1};
-            int currentWinnerScore = 0;
-            for (int i = 0; i < data.length; i++) {
-                for (int j = 0; j < data[i].length; j++) {
-                    if (selected[i][j]) {
-                        continue;
-                    }
-                    int currentScore = 0;
-                    for (int k = 0; k < 110; k++) {
-                        if (k==0) {
-                            if (!processed[i][j] && data[i][j]) {
-                                currentScore = 1;
-                            }
-                        } else {
-                            if (!inBounds(i, j, k)) break;
-                            if (!isWorth(k, currentScore)) {
-                                break;
-                            }
-
-                            int kk = 2 * k + 1;
-                            int scp = 0;
-                            int scn = 0;
-                            for (int ii = -k; ii <= k; ii++) {
-                                if (!processed[i - ii][j - k]) {
-                                    if (data[i - ii][j - k]) {
-                                        scp++;
-                                    } else {
-                                        scn++;
-                                    }
-                                }
-                            }
-                            for (int jj = -k + 1; jj < k; jj++) {
-                                if (!processed[i - k][j - jj]) {
-                                    if (data[i - k][j - jj]) {
-                                        scp++;
-                                    } else {
-                                        scn++;
-                                    }
-                                }
-                            }
-                            currentScore += scp - (10 * scn) - 1;
+        for (int mm = 9; mm >= 1 ; mm-=2) {
+            while(!finished()) {
+                int[] currentWinner = new int[]{-1,-1,-1};
+                int currentWinnerScore = 0;
+                for (int i = 0; i < data.length; i++) {
+                    for (int j = 0; j < data[i].length; j++) {
+                        if (selected[i][j]) {
+                            continue;
                         }
+                        int currentScore = 0;
+                        for (int k = 0; k < 110; k++) {
+                            if (k==0) {
+                                if (!processed[i][j] && data[i][j]) {
+                                    currentScore = 1;
+                                }
+                            } else {
+                                if (!inBounds(i, j, k)) break;
+                                if (!isWorth(k, currentScore)) {
+                                    break;
+                                }
 
-                        if (currentScore > currentWinnerScore) {
-                            currentWinnerScore = currentScore;
-                            currentWinner = new int[]{i, j, k};
+                                int kk = 2 * k + 1;
+                                int scp = 0;
+                                int scn = 0;
+                                for (int ii = -k; ii <= k; ii++) {
+                                    if (!processed[i - ii][j - k]) {
+                                        if (data[i - ii][j - k]) {
+                                            scp++;
+                                        } else {
+                                            scn++;
+                                        }
+                                    }
+                                }
+                                for (int jj = -k + 1; jj < k; jj++) {
+                                    if (!processed[i - k][j - jj]) {
+                                        if (data[i - k][j - jj]) {
+                                            scp++;
+                                        } else {
+                                            scn++;
+                                        }
+                                    }
+                                }
+                                currentScore += scp - (mm * scn) - 1;
+                            }
+                            if (currentScore > currentWinnerScore) {
+                                currentWinnerScore = currentScore;
+                                currentWinner = new int[]{i, j, k};
+                            }
                         }
                     }
                 }
+
+                if (currentWinner[0]<0) break;
+                System.out.println(currentWinner[0] + "," + currentWinner[1] + "," + currentWinner[2]+"["+currentWinnerScore+"] m {["+mm+"]}");
+                selected(currentWinner[0], currentWinner[1], currentWinner[2]);
+                if (currentWinner[2]==0) break;
             }
 
-            if (currentWinner[0]<0) break;
-            System.out.println(currentWinner[0] + "," + currentWinner[1] + "," + currentWinner[2]+"["+currentWinnerScore+"]");
-            selected(currentWinner[0], currentWinner[1], currentWinner[2]);
-            if (currentWinner[2]==0) break;
         }
-
         if (!finished()) {
             for (int i = 0; i < data.length; i++) {
                 for (int j = 0; j < data[i].length; j++) {
